3d12db85c394e577be76425e5c9456488eb2418f
venky-at-034995048232 <venkatesh.yanamaddi3@gmail.com>
2024-11-09 13:34:29 +0530

    nk

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const port=process.env.port || 5001;
 const path = require('path');
 
 app.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://medscore-api.onrender.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // If you're using cookies or authentication
+    maxAge: 86400 // How long the results of a preflight request can be cached
+  };
+  
+  app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'build')));
 app.use("/api/user",require("./Router/registerRoutes"))
 
